f793336112b0afd152df732197633f5b5b1a4ad1
el barrera <samuelbarrerapatino2403@gmail.com>
2024-07-11 10:43:09 -0500

    dockerfile

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import {config} from 'dotenv';
 import rout from "./routes/index.js";
+config();
 
 const server = express();
 
-server.set('port', process.env.PORT || 4550);
+server.set('port', process.env.PORT || 4551);
 
 server.get('/',rout)
 
